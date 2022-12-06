d1f4f1dbc4dffd2cc31073440eff53259d470359
emivai <e.vaisva@gmail.com>
2022-12-06 14:10:20 +0200

    Add exception hadnling middleware and unit test

---
+++ tests/Adform.Academy.Kudos.Tests/Services/EmployeeServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Adform.Academy.Core.Contracts.Repositories;
+using Adform.Academy.Core.Entities;
+using Adform.Academy.Kudos.Application.Services;
+using AutoFixture.NUnit3;
+using Moq;
+
+namespace Adform.Academy.Kudos.Tests.Services
+{
+    public class EmployeeServiceTests
+    {
+        private readonly Mock<IEmployeeRepository> _employeeRepositoryMock = new();
+        private EmployeeService _employeeService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _employeeService = new EmployeeService(_employeeRepositoryMock.Object);
+        }
+
+        [Test, AutoData]
+        public async Task GetAsync_EmployeesExist_ShouldReturnData(Task<List<Employee>> employees)
+        {
+
+            _employeeRepositoryMock.Setup(x => x.GetAsync()).Returns(employees);
+
+            var result = await _employeeService.GetAsync();
+
+            Assert.IsNotEmpty(result);
+        }
+    }
+}
